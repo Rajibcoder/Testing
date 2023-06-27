f8537a286b5dbb9bac418c81b5ace664c51e4c15
Rajibcoder <rajibkumarmaity1110@gmail.com>
2023-06-27 15:44:57 +0530

    ps1

---
+++ TypeCastingDemo.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class TypeCastingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		double num2 = num;
+		System.out.println(num2);
+		
+		double num3 = 9.8;
+		int num4 = (int)(num3);
+		System.out.println(num4);
+	}
+
+}
