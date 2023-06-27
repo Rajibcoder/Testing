c7ccbe25e6e3f62e7ace9ea67643ff507df1df9b
Rajibcoder <rajibkumarmaity1110@gmail.com>
2023-06-27 16:16:06 +0530

    ps1

---
+++ src/first/TypeCasting.java
@@ -0,0 +1,19 @@
+package first;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Implicit type casting..");
+		int a = 10;
+		double b = a;
+		System.out.println(b);
+		
+		System.out.println("Explicit type casting..");
+		double c = 9.6;
+		int d = (int)(c);
+		System.out.println(d);
+	}
+
+}
